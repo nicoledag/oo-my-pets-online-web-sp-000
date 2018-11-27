7685c0c4f3b2f42a425c1bcc17628772415b6779
Nicole D <nnardella@gmail.com>
2018-11-27 02:14:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -71,7 +71,7 @@ class Owner
 
   def list_pets
     binding.pry
-    pets.each do |type, pet| puts "I have #pet[:fishes].count} fish, pet.count dog(s), and pet.count cat(s)."
+    pets.each do |type, pet| puts pet.count
     end
     puts "I have #{pet}.count fish, 3 dog(s), and 1 cat(s)."
   end
